c8ee24b3245aac65f9327b5f777a87e32182d326
andrewscarlos <andrews.knct@gmail.com>
2022-01-11 17:00:22 -0300

    feat: added postgres on docker and create comunication about app with postgres database

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { router } from "./routes";
 import swaggerUi from "swagger-ui-express";
 import swaggerFIle from "./swagger.json";
+import './database';
 const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFIle));
